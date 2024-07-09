135f0f00e88636f66aeeea97bfb831791b8e7336
Rodney Gauna <rodneygauna@gmail.com>
2024-07-09 11:10:00 -0700

    frontend login added (for testing)

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,23 @@
 import express from "express";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 
 const app = express();
 
 // Middleware
+const allowedOrigins = ["http://example.com", "http://localhost:3000"];
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+  })
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
